6c400d1f6e7006d40dbca61244a5344ecc0845c6
Barkerx <ziegler_bhk@hotmail.com>
2018-11-08 08:53:05 -0300

    1.02 fix 5
    -Al ganar aparece correctamente la imagen de ganado, y ahora se dispara de forma normal(se quitaron algunos metodos y atributos no usados).
    
    -Mostrar vida y puntaje es necesario. proximo detalle sera este.

---
+++ src/resources/gui/Juego.java
@@ -0,0 +1,151 @@
+package gui;
+
+import java.awt.event.KeyEvent;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+import mapa.*;
+import misc.Unidad;
+import misc.nave;
+import Player.jugador;
+
+public  class Juego {
+	private jugador j;
+	private static gui frame;
+	private Mapa m;
+	private int n;
+	private boolean gane=false;
+	private boolean habilitado=false;
+	public static void main(String[] args) {
+		new Juego();
+		
+	
+}
+/**
+ * constructor de juego, crea el jugador en nulo crea el mapa y pone la gui visible.	
+ */
+public Juego(){
+		n=1;
+		j=new jugador(null,null);
+		frame=new gui(this);
+		m=new mapa1(frame,this,j);
+		frame.setVisible(true);
+		j.setMap(m);
+		j.initgraph();
+   }
+   
+   
+   
+  
+  /**
+   * metodo usado para mover el jugador a partir de un n desde la gui
+   * @param dir direccion donde se movera el jugador
+   */
+
+public void mover(int dir){
+	
+   int direccion = 0;
+   if(j.getIsRunning()&&habilitado){
+	switch (dir){
+		case KeyEvent.VK_UP : //Arriba
+			direccion = Unidad.ARRIBA;
+			break;
+		case KeyEvent.VK_LEFT : //Izquierda
+			direccion = Unidad.IZQUIERDA;
+			break;
+		case KeyEvent.VK_DOWN : //Abajo
+			direccion = Unidad.ABAJO;
+			break;	
+		case KeyEvent.VK_RIGHT : //Derecha
+			direccion = Unidad.DERECHA;
+			break;
+		case KeyEvent.VK_SPACE : //Espacio
+			direccion= nave.DISPARAR;
+			break;
+	}
+	
+	j.mover(direccion);
+   }
+}
+/**
+ * metodo usado para aumentar los puntos del jugador
+ * @param getpuntos puntos a sumar en el jugador
+ */
+public void addpuntos(int getpuntos) {
+	j.sumarPuntaje(getpuntos);
+	
+}
+/**
+ * Metodo usado cuando se gana el juego
+ */
+public void win() {
+		n=n+1;
+	if(!gane){	
+		if(n==2)
+			Mapa2();
+		//if(n==3)
+			//MapaBoss();
+		if(n==3){
+			System.out.println("GANASTE n es 3");
+			
+			gane=true;
+			JLabel stat =new JLabel(new ImageIcon(this.getClass().getResource("/resources/winimage.png")));
+			stat.setBounds(0, 0, 900, 675);
+			frame.add(stat, new Integer(1));	//hacer un frame de ganaste.
+		}
+	}
+	
+}
+/*
+private void MapaBoss(){
+	if(n==3){
+		m=null;
+		j.setCelda(null);
+		j.setMap(null);
+		frame.dispose();
+		frame=new gui(this);
+		m=new mapaBoss(frame,this,j);
+		frame.setVisible(true);
+		j.setMap(m);
+		j.initgraph();
+	}
+}*/
+private void Mapa2(){
+	if(n==2){
+	j.setCelda(null);
+	j.setMap(null);
+	frame.dispose();
+	m=null;
+	frame=new gui(this);
+	m=new mapa2(frame,this,j);
+	frame.setVisible(true);
+	j.setMap(m);
+	j.initgraph();
+	}
+}
+
+public void gamerover() {
+	System.out.println("Perdiste");
+	JLabel stat =new JLabel(new ImageIcon(this.getClass().getResource("/resources/gamerover.png")));
+	stat.setBounds(0, 0, 900, 675);
+	frame.add(stat, new Integer(1));
+	//hacer un frame de perdiste.
+	habilitado=false; 
+}
+
+public void habilitado(){
+	habilitado=true;
+}
+public void deshabilitado(){
+	habilitado=false;
+}
+public boolean puedo() {
+	return habilitado;
+}
+public jugador getJugador(){
+	return j;
+}
+
+
+}
---
+++ src/resources/gui/gui.java
@@ -0,0 +1,122 @@
+package gui;
+import javax.swing.border.EmptyBorder;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.JLayeredPane;
+import javax.swing.*;
+
+
+public class gui extends JFrame implements Runnable {
+	private static final long serialVersionUID = 1L;
+
+	private JLayeredPane contentPane;
+    private Thread t;
+	protected boolean keyBoardUpPressed;
+	protected boolean keyBoardDownPressed;
+	protected boolean keyBoardLeftPressed;
+	protected boolean keyBoardRightPressed;
+	
+	private static Juego j;
+
+	public gui(final Juego j) {	
+		super("galaga");
+       addKeyListener(new KeyAdapter() {
+			
+			public void keyPressed(KeyEvent arg0) {
+				if(arg0.getKeyCode()==KeyEvent.VK_UP &&j.puedo()){
+					keyBoardUpPressed=true;
+				}
+				else
+					if(arg0.getKeyCode()==KeyEvent.VK_DOWN&&j.puedo()){
+						keyBoardDownPressed=true;
+					}
+					else
+						if(arg0.getKeyCode()==KeyEvent.VK_LEFT&&j.puedo()){
+							keyBoardLeftPressed=true;
+						}
+						else
+							if(arg0.getKeyCode()==KeyEvent.VK_RIGHT&&j.puedo()){
+								keyBoardRightPressed=true;
+							}
+							else
+								if(arg0.getKeyCode()==KeyEvent.VK_SPACE&&j.puedo())
+									j.mover(KeyEvent.VK_SPACE);				
+			}
+			public void keyReleased(KeyEvent arg0){
+				if(arg0.getKeyCode()==KeyEvent.VK_UP&&j.puedo()){
+					keyBoardUpPressed=false;
+				}
+				else
+					if(arg0.getKeyCode()==KeyEvent.VK_DOWN&&j.puedo()){
+						keyBoardDownPressed=false;
+					}
+					else
+						if(arg0.getKeyCode()==KeyEvent.VK_LEFT&&j.puedo()){
+							keyBoardLeftPressed=false;
+						}
+						else
+							if(arg0.getKeyCode()==KeyEvent.VK_RIGHT&&j.puedo()){
+								keyBoardRightPressed=false;
+							}
+							
+			}
+			
+				
+    });
+        //crea la ventana de la gui.
+		getContentPane().setLayout(null);	
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(0, 0, 910, 710);
+		setLocationRelativeTo(null);
+		contentPane = new JLayeredPane();
+		contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		//inicializa la gui
+		
+		t=new Thread(this);
+		t.start();
+		gui.j=j;
+		
+		
+	}
+	
+private void mover(){
+	 if(keyBoardLeftPressed&&j.puedo()){
+     	  j.mover(KeyEvent.VK_LEFT);
+       }
+       else
+     	  if(keyBoardRightPressed&&j.puedo()){
+     		  j.mover(KeyEvent.VK_RIGHT);
+           }
+           else
+         	  if(keyBoardDownPressed&&j.puedo()){
+         		  j.mover(KeyEvent.VK_DOWN);
+               }
+               else
+             	  if(keyBoardUpPressed&&j.puedo()){
+             		  j.mover(KeyEvent.VK_UP);
+                   }
+}
+/**
+ * Metodo que se ejecutara siempre al crear la gui.
+ */
+public void run() {
+	while(true){
+			
+              mover();		  
+                   
+		try {
+			Thread.sleep(150);
+		} catch (InterruptedException e) {
+			
+			e.printStackTrace();
+		}
+	}
+	
+}
+
+}
+
